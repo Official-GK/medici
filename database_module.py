9099c7e23a721623661b0d6a917b75716bf76726
GK <gauravkul2006@gmail.com>
2024-12-27 16:50:58 +0530

    Update database_module.py

--- database_module.py
+++ database_module.py
@@ -9,5 +9,6 @@ users_collection = db['users']
 def save_user_data(data):
     users_collection.insert_one(data)
 
+
 def get_user_data(user_id):
     return users_collection.find_one({"user_id": user_id})
